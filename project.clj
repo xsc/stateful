3fdcf3439893e38aa38744aa617d62070bdc9523
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-01-07 12:21:46 +0100

    rename artifact to 'xsc/stateful'.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stateful-generators "0.1.0-SNAPSHOT"
+(defproject xsc/stateful "0.1.0-SNAPSHOT"
   :description "Stateful Generators for Clojure's test.check"
   :url "https://github.com/xsc/stateful-generators"
   :license {:name "MIT License"
